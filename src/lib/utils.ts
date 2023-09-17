1b1906631b4861125d987beee9db337df8dd0a49
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:12:01 +0500

    utils file added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
+import { cdnClient } from '@/lib/sanityClient';
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import imageUrlBuilder from "@sanity/image-url";
+
+
+
+const builder = imageUrlBuilder(cdnClient);
+
+export const urlFor = (source: any) => builder.image(source);
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
