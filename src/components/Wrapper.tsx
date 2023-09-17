3d637f17a53c58ef285c030b8357881a50e5a32b
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:10:14 +0500

    Wrapper file update

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from "react";
 
-const Wrapper = ({children}:{children:ReactNode}) => {
-  return <div className="w-full max-w-[1200px] flex flex-col items-center justify-center ">
-    {children}
-  </div>;
+const Wrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex w-full max-w-[1200px] flex-col items-center justify-center ">
+      {children}
+    </div>
+  );
 };
 
 export default Wrapper;
