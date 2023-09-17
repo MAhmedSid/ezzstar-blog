8a417ea547aff6b7a3aa137df5c9ea5c05e51521
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:14:35 +0500

    HOME Latest blogs comp updated

--- src/views/HOME/LatestUpdates.tsx
+++ src/views/HOME/LatestUpdates.tsx
@@ -2,17 +2,24 @@ import BlogCover from "@/components/BlogCover";
 import Wrapper from "@/components/Wrapper";
 import React from "react";
 
-const LatestUpdates = () => {
-  return (
-    <section className="flex flex-col w-full justify-center items-center">
+const LatestUpdates = async (blogData:any) => {
+
+    const data = blogData.blogData;
+
+    return (
+      <section className="flex flex-col w-full justify-center items-center">
       <Wrapper>
         <div className="flex flex-col gap-y-4">
           <h2 className="text-2xl font-bold text-center tablet:text-left">LATEST UPDATES</h2>
           <div className="w-full flex flex-col tablet:flex-row px-2 gap-y-2 tablet:gap-x-5 justify-center">
             <div className="flex flex-col gap-y-3 items-center tablet:items-start ">
-              <BlogCover />
-              <BlogCover />
-              <BlogCover />
+              {data && data.map((blog : any ,i : any)=>{
+                  return(
+                  
+             (blog && <BlogCover key={blog?.slug.current} title={blog?.title} slug={blog?.slug.current} desc={blog?.meta_desc} likesCount={blog?.likesCount}  date={blog?.published_at} category={blog?.category} imgSrc={blog?.displayImg?.asset} imgAlt={blog.displayImg?.alt} />)
+                  
+                  )
+              })}
             </div>
             <div className="h-[100px] w-full tablet:h-[600px] tablet:w-[200px] bg-slate-500"></div>
           </div>
