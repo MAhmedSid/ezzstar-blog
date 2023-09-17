191d87178f5998b0a185b8cff363812d57202981
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:11:16 +0500

    Navbar updated

--- src/views/Navbar/MobNavMenu.tsx
+++ src/views/Navbar/MobNavMenu.tsx
@@ -1,18 +1,45 @@
-'use client'
-import React, { useState, useRef } from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { X, Menu } from 'lucide-react'
-import Link from 'next/link'
+"use client";
+import React, { useState, useRef } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { X, Menu } from "lucide-react";
+import Link from "next/link";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { toast } from "react-hot-toast";
+import { removeSession } from "@/store/session/sessionReducer";
+import useSaveSession from "@/hooks/useSaveSession";
 
 const MobNavMenu = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const toggleMenu = () => {
     if (open) {
-      setOpen(false)
+      setOpen(false);
     } else {
-      setOpen(true)
+      setOpen(true);
     }
-  }
+  };
+
+  const handleSignout = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        toast.error(error.message);
+      }
+      toast.success("You have Suxxessfully logged out.");
+      dispatch(removeSession);
+      window.location.reload();
+    } catch (error) {}
+  };
+
+  const supabase = createClientComponentClient();
+
+  const userId = useSaveSession();
+  const dispatch = useAppDispatch();
 
   return (
     <DropdownMenu onOpenChange={toggleMenu}>
@@ -20,7 +47,7 @@ const MobNavMenu = () => {
         {open === false ? (
           <Menu className="h-9  w-9 flex-[0.1] rounded-md border-2 border-pri_blue stroke-pri_blue  animate-in" />
         ) : (
-          <X  className="h-9 w-9  flex-[0.1] rounded-md border-2 border-pri_blue stroke-pri_blue  animate-in" />
+          <X className="h-9 w-9  flex-[0.1] rounded-md border-2 border-pri_blue stroke-pri_blue  animate-in" />
         )}
       </DropdownMenuTrigger>
 
@@ -28,28 +55,61 @@ const MobNavMenu = () => {
         <div className=" bg-gradient-to-b from-pri_yellow  to-pri_blue p-[2px]">
           <div className="flex flex-col items-center justify-center rounded-md bg-black text-white  child-hover:text-pri_yellow ">
             <DropdownMenuItem>
-              <Link href={'/'} className='text-lg' >HOME</Link>
+              <Link href={"/"} className="text-lg">
+                HOME
+              </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Link href={'/world'} className='text-lg' >GAMES</Link>
+              <Link href={"/gaming"} className="text-lg">
+                GAMES
+              </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Link href={'/nft'} className='text-lg' >ANIME</Link>
+              <Link href={"/anime"} className="text-lg">
+                ANIME
+              </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Link href={'/events'} className='text-lg' >BLOGS</Link>
+              <Link href={"/blogs"} className="text-lg">
+                BLOGS
+              </Link>
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Link href={'/merch'} className='text-lg' >EVENTS</Link>
+              <Link href={"/events"} className="text-lg">
+                EVENTS
+              </Link>
             </DropdownMenuItem>
+
             <DropdownMenuItem>
-              <Link href={'/future'} className='text-lg' >ACCOUNT</Link>
+              {userId === "" ? (
+                <Link href={"/account/signin/#signin"} className="text-lg">
+                  Sign In
+                </Link>
+              ) : (
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <p className="text-lg">ACCOUNT</p>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <div className="w-full bg-gradient-to-b from-pri_yellow  to-pri_blue p-[2px]">
+                      <div className="flex flex-col items-center  justify-center   rounded-md bg-black px-4 py-2 text-white  child-hover:text-pri_yellow">
+                        <Link href={"/account/setting/#setting"} className="text-lg">
+                          Setting
+                        </Link>
+                        <button onClick={handleSignout} className="text-lg">
+                          Sign Out
+                        </button>
+                      </div>
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
             </DropdownMenuItem>
           </div>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default MobNavMenu
+export default MobNavMenu;
--- src/views/Navbar/Navbar.tsx
+++ src/views/Navbar/Navbar.tsx
@@ -1,24 +1,56 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
-import logo from '/public/images/logo.webp'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import MobNavMenu from './MobNavMenu'
+"use client";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import logo from "/public/images/logo.webp";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import MobNavMenu from "./MobNavMenu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { toast } from "react-hot-toast";
+import { removeSession } from "@/store/session/sessionReducer";
+import useSaveSession from "@/hooks/useSaveSession";
 
 const Navbar = () => {
-  const path = usePathname()
+  const userId = useSaveSession();
+
+  const supabase = createClientComponentClient();
+  const dispatch = useAppDispatch();
+  const path = usePathname();
+
+  const handleSignout = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        toast.error(error.message);
+      }
+      toast.success("You have Suxxessfully logged out.");
+      dispatch(removeSession);
+      window.location.reload();
+    } catch (error) {}
+  };
 
   return (
-    <header className="flex w-full items-center justify-between gap-y-2 px-6 py-4 tablet:gap-x-10 lcd:gap-x-24 font-medium text-white  tablet:justify-center lp:px-24 lp:text-lg">
+    <header className="flex w-full items-center justify-between gap-y-2 px-6 py-4 font-medium text-white tablet:justify-center tablet:gap-x-10  lp:px-24 lp:text-lg lcd:gap-x-24">
       <nav className=" hidden gap-x-4 child-hover:text-pri_yellow tablet:flex  lp:gap-x-12 lcd:gap-x-16 child:lcd:text-lg">
-        <Link href={'/'} className={`${path === '/' && 'text-pri_yellow'}`}>
+        <Link href={"/"} className={`${path === "/" && "text-pri_yellow"}`}>
           HOME
         </Link>
-        <Link href={'/gaming'} className={`${path === '/world' && 'text-pri_yellow'}`}>
+        <Link
+          href={"/gaming"}
+          className={`${path === "/world" && "text-pri_yellow"}`}
+        >
           GAMES
         </Link>
-        <Link href={'/anime'} className={`${path === '/nft' && 'text-pri_yellow'}`}>
+        <Link
+          href={"/anime"}
+          className={`${path === "/nft" && "text-pri_yellow"}`}
+        >
           ANIME
         </Link>
       </nav>
@@ -30,19 +62,45 @@ const Navbar = () => {
       />
 
       <nav className=" hidden gap-x-4 child-hover:text-pri_yellow tablet:flex  lp:gap-x-12 lcd:gap-x-16 child:lcd:text-lg">
-        <Link href={'/blogs'} className={`${path === '/events' && 'text-pri_yellow'}`}>
+        <Link
+          href={"/blogs"}
+          className={`${path === "/events" && "text-pri_yellow"}`}
+        >
           BLOGS
         </Link>
-        <Link href={'/events'} className={`${path === '/merch' && 'text-pri_yellow'}`}>
+        <Link
+          href={"/events"}
+          className={`${path === "/merch" && "text-pri_yellow"}`}
+        >
           EVENTS
         </Link>
-        <Link href={'/account'} className={`${path === '/future' && 'text-pri_yellow'}`}>
-          ACCOUNT
-        </Link>
+        {userId === "" ? (
+          <Link href={"/account/signin/#signin"}>Sign In</Link>
+        ) : (
+          <Popover>
+            <PopoverTrigger>
+              <p className={`${path === "/future" && "text-pri_yellow"}`}>
+                ACCOUNT
+              </p>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className=" rounded-md bg-gradient-to-b from-pri_yellow  to-pri_blue p-[2px]">
+                <div className="flex flex-col items-center justify-center rounded-md bg-black py-2  text-white child-hover:text-pri_yellow ">
+                  <Link href={"/account/setting/#setting"} className="text-lg">
+                    Setting
+                  </Link>
+                  <button onClick={handleSignout} className="text-lg">
+                    LOG OUT
+                  </button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        )}
       </nav>
       <MobNavMenu />
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
