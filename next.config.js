32fc8105f1ed0634da9e718ba1395c938c429979
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:05:37 +0500

    next js config update.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: {
+const nextConfig = {
+  experimental: {
     serverActions: true,
-  },}
+  },
+  images: {
+    formats: ["image/webp", "image/avif"],
+    domains: ["images.ctfassets.net", "cdn.sanity.io","ydnjcrmmrqnylligfbpk.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
