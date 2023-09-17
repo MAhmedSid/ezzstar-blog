01788f5e7dd16a68a1a5d568340dfb4e5860247e
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:14:06 +0500

    HOME Blog comp updated

--- src/views/HOME/Blogs.tsx
+++ src/views/HOME/Blogs.tsx
@@ -2,26 +2,31 @@ import BlogCover from "@/components/BlogCover";
 import Wrapper from "@/components/Wrapper";
 import React from "react";
 
-const Blogs = () => {
+const Blogs = async (blogData:any) => {
+
+  const data = blogData.blogData;
+
   return (
-    <section className="flex flex-col w-full gap-y-10 justify-center items-center">
-      <Wrapper>
-        <div className="flex flex-col gap-y-4">
-          <h2 className="text-2xl font-bold text-center tablet:text-left">BLOGS</h2>
-          <div className="w-full flex flex-col tablet:flex-row px-2 gap-y-2 tablet:gap-x-5 justify-center">
+    <section className="flex flex-col w-full justify-center items-center">
+    <Wrapper>
+      <div className="flex flex-col gap-y-4">
+        <h2 className="text-2xl font-bold text-center tablet:text-left">BLOGS</h2>
+        <div className="w-full flex flex-col tablet:flex-row px-2 gap-y-2 tablet:gap-x-5 justify-center">
             <div className="h-[100px] w-full tablet:h-[600px] tablet:w-[200px] bg-slate-500"></div>
-            <div className="flex flex-col gap-y-3 items-center tablet:items-start ">
-              <BlogCover />
-              <BlogCover />
-              <BlogCover />
-            </div>
+          <div className="flex flex-col gap-y-3 items-center tablet:items-start ">
+            {data && data.map((blog : any ,i : any)=>{
+                return(
+                
+           (blog && <BlogCover key={blog?.slug.current} title={blog?.title} slug={blog?.slug.current} desc={blog?.meta_desc} likesCount={blog?.likesCount}  date={blog?.published_at} category={blog?.category} imgSrc={blog?.displayImg?.asset} imgAlt={blog.displayImg?.alt} />)
+                
+                )
+            })}
           </div>
         </div>
-      </Wrapper>
-      <button className="text-lg font-semibold bg-pri_purple rounded-full px-8 py-1">View More</button>
-
-    </section>
-  );
+      </div>
+    </Wrapper>
+  </section>
+);
 };
 
 export default Blogs;
