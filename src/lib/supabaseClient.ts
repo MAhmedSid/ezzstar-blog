5419e75a002efe1a740bce2e5e954e95312f7297
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-17 20:10:38 +0500

    supabase client added

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient("https://ydnjcrmmrqnylligfbpk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlkbmpjcm1tcnFueWxsaWdmYnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMTU4NjQsImV4cCI6MjAwODg5MTg2NH0.KxVhlE-UaIF2wJvzhk6KHnbeqQ-R6Tu2AGWK7ldfOmc", {
+export const supabase = createClient( process.env.NEXT_PUBLIC_SUPABASE_URL! , process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
