b86563af8bf428c40e6f0e4389eb88872b80d4be
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-24 16:29:47 +0500

    fix nested layout bug

--- src/app/account/signup/layout.tsx
+++ src/app/account/signup/layout.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/views/Footer";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
