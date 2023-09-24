3cbcaaf25e8f8798d6f25b03433da8847db16454
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-24 09:10:01 +0500

    error fixing commit from comment section

--- src/views/CommentSection/CommentSec.tsx
+++ src/views/CommentSection/CommentSec.tsx
@@ -7,7 +7,7 @@ import AddCommentComp from "@/components/AddCommentComp";
 
 const CommentSec = async ({blogId,blogSlug}:{blogSlug:string,blogId:string}) => {
 
-    const res = await fetch(`https://www.ezzstar.com/api/getComments`,{
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getComments`,{
       method: "PUT",
       body: JSON.stringify({ blogId }),
       headers: { "Content-Type": "application/json" },
@@ -22,15 +22,15 @@ const CommentSec = async ({blogId,blogSlug}:{blogSlug:string,blogId:string}) => 
   return (
     <section className="flex h-full w-full flex-col gap-y-5">
       
-      {/* <AddCommentComp blogSlug={blogSlug} blogId={blogId} /> */}
+      <AddCommentComp blogSlug={blogSlug} blogId={blogId} />
       <section className="flex flex-col bg-zinc-950 p-4 gap-y-10 rounded-xl">
         <p>{comments && comments.length} {comments && comments.length > 1 ? "Comments": "Comment"}</p>
-        {/* <div className="h-full w-full flex flex-col gap-y-8">
+        <div className="h-full w-full flex flex-col gap-y-8">
           {comments && comments.map((comment:any,i:number)=>(
             <Comment key={comment._id} comments={comments} commentUserId={comment.userId} replies={comment?.replies} commentId={comment._id} text={comment.commentText} createdAt={comment._createdAt} blogId={blogId}  />
           ))}
           {comments && comments.length === 0 && <p className="text-xl text-center">No Comments Yet!</p> }
-        </div> */}
+        </div>
       </section>
     </section>
   );
