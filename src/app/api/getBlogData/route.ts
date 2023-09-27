3928984b95d7dcf1feda5afb9d86172e6c950bdd
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-09-27 19:46:48 +0500

    api route created for post body content.

---
+++ src/app/api/getBlogData/route.ts
@@ -0,0 +1,58 @@
+import { cdnClient } from "@/lib/sanityClient";
+import { NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { NextResponse } from "next/server";
+
+export async function PUT(req: Request, res: NextApiResponse) {
+  try {
+    if (req.body) {
+      const body = await req.json();
+      const { slug ,cat} = body;
+
+      const res = await cdnClient.fetch(groq`{
+        "blogData": *[_type == "blogs" && slug.current == '${slug}'][0]{
+      title,
+      category,
+      published_at,
+      slug,
+      _id,
+      pageContent[]{
+        ...,
+        image {
+          alt,
+          "url": asset->url
+        }
+      },
+      "likesCount": length(likes),
+    },
+        "commentNumber": count(*[_type == "comments" && blogSlug == '${slug}'])
+        ,
+        "morePost": *[_type == "blogs" ${
+          cat !== "" && cat !== "undefined" ? `&& category == '${cat}'` : ""
+        } ]  | order(published_at desc)  [0...3]{
+          title,
+          category,
+          published_at,
+          slug,
+          displayImg,
+          "likesCount": length(likes),
+        }
+      }`);
+      return NextResponse.json(
+        {
+          message: "Sent Comments",
+          data: res,
+        },
+        { status: 200 },
+      );
+
+    } else {
+      return NextResponse.json({ message: "Bad request" }, { status: 400 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as { message: string }).message },
+      { status: 500 },
+    );
+  }
+}
