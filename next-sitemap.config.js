005fdfe49e724d0227ea58e688722faef508d69a
MAhmedSid <mahmedsiddiqui333@gmail.com>
2023-10-09 17:05:02 +0500

    updated next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl:  'https://ezzstar.com',
+    changefreq:"daily",
     generateRobotsTxt: true, 
     exclude: ['/account/*','/api/*','/server-sitemap.xml','/post/*','/studio'],
    robotsTxtOptions:{
